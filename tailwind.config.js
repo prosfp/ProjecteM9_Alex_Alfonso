0404e3219b0418a5eef863c33cecd32e006cc1e7
Alex Loureda <alex.loureda@cirvianum.cat>
2024-10-24 16:06:07 +0200

    Co-authored-by: Alfonso1234e2ijn <Alfonso1234e2ijn@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html", "./index.html"],
+  content: ["./public/**/*.html", "./src/**/*.js", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
+    colors: {
+      'primary': '#f5f5f5', // bg-gray-100
+      'secondary1': '#86EFAC', // bg-green-300
+      'secondary2': '#93C5FD', // bg-blue-300
+      'tertiary': '#1A3D40',
+    },
   },
   plugins: [],
 };
