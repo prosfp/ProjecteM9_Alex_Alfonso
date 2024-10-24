542b5271204ac3e78809822708b7fcbca2002623
PABLO ROS PROFE <pros7@xtec.cat>
2024-10-24 16:36:59 +0200

    canvi estructura projecte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html", "./src/**/*.js", "./index.html"],
+  content: [
+    "./public/**/*.html", // Scan HTML files in public folder
+    "./src/**/*.{html,js}", // Scan HTML and JS files in src
+  ],
   theme: {
-    extend: {
-      
-    },
+    extend: {},
     colors: {
-      'primary': '#f5f5f5', // bg-gray-100
-      'secondary1': '#86EFAC', // bg-green-300
-      'secondary2': '#93C5FD', // bg-blue-300
-      'tertiary': '#1A3D40',
+      primary: "#f5f5f5", // bg-gray-100
+      secondary1: "#86EFAC", // bg-green-300
+      secondary2: "#93C5FD", // bg-blue-300
+      tertiary: "#1A3D40",
     },
   },
   plugins: [],
