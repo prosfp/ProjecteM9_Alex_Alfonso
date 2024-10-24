f22eec4e3963edc84c17e33f6f03891e1ecd426b
PABLO ROS PROFE <pros7@xtec.cat>
2024-10-24 17:17:29 +0200

    repositori tipus projecte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,46 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./public/**/*.html", // Scan HTML files in public folder
     "./src/**/*.{html,js}", // Scan HTML and JS files in src
   ],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#f5f5f5", // bg-gray-100
-      secondary1: "#86EFAC", // bg-green-300
-      secondary2: "#93C5FD", // bg-blue-300
-      tertiary: "#1A3D40",
+    extend: {
+      colors: {
+        primary: "#f5f5f5", // bg-gray-100
+        secondary1: "#86EFAC", // bg-green-300
+        secondary2: "#93C5FD", // bg-blue-300
+        tertiary: "#1A3D40",
+      },
     },
   },
   plugins: [],
 };
+
+// extend: {
+//   spacing: {
+//     '128': '32rem',
+//     '144': '36rem',
+//   },
+//   borderRadius: {
+//     '4xl': '2rem',
+//   },
+//   fontFamily: {
+//     sans: ['Graphik', 'sans-serif'],
+//     serif: ['Merriweather', 'serif'],
+//   },
+//   fontSize: {
+//     'xs': '.75rem',
+//     'sm': '.875rem',
+//     'tiny': '.875rem',
+//     'base': '1rem',
+//     'lg': '1.125rem',
+//     'xl': '1.25rem',
+//     '2xl': '1.5rem',
+//     '3xl': '1.875rem',
+//     '4xl': '2.25rem',
+//     '5xl': '3rem',
+//     '6xl': '4rem',
+//     '7xl': '5rem',
+//   },
+// },
